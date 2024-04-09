c35b5dcf1d761c8361dfc14c8ccea06c45d5c735
Marco Gonzalez <mgonzalez11@hawk.iit.edu>
2024-04-09 15:44:43 -0500

    Initial Commit

---
+++ Bifurcation Diagram/Bifurcation_DiagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bifurcation_DiagramApp.swift
+//  Bifurcation Diagram
+//
+//  Created by Marco Gonzalez on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Bifurcation_DiagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
