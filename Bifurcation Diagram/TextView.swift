4872d63bede397d188c969bbc06cea663d157d50
Marco Gonzalez <mgonzalez11@hawk.iit.edu>
2024-04-26 10:58:27 -0500

    Changes were made.

--- Bifurcation Diagram/TextView.swift
+++ Bifurcation Diagram/TextView.swift
@@ -1,3 +1,11 @@
+//
+//  TextView.swift
+//  Bifurcation Diagram
+//
+//  Created by Jeff_Terry on 1/15/24.
+//  Modified by Marco Gonzalez 2/11/24
+//
+
 import SwiftUI
 import UniformTypeIdentifiers
 
